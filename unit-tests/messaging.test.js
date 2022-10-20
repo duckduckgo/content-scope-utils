0e88731ebd8a94f35c331201daf159a060a933cf
Shane Osbourne <shane.osbourne8@gmail.com>
2022-10-20 13:48:16 +0100

    don't put test files inside lib

--- unit-tests/messaging.test.js
+++ unit-tests/messaging.test.js
@@ -1,7 +1,7 @@
 import { expect } from "@esm-bundle/chai";
-import { Messaging } from "../messaging.js";
-import { WindowsMessagingConfig, WindowsMessagingTransport } from "./windows.js";
-import { WebkitMessagingConfig, WebkitMessagingTransport } from "./webkit.js";
+import { Messaging } from "../lib/messaging.js";
+import { WindowsMessagingConfig, WindowsMessagingTransport } from "../lib/messaging/windows.js";
+import { WebkitMessagingConfig, WebkitMessagingTransport } from "../lib/messaging/webkit.js";
 
 it("can construct webkit messaging", async () => {
   const config = new WebkitMessagingConfig({
